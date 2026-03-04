6cf402c083ab6d7a93215b64b975b8f320fcea9f
Kamil Tusiński <kamil.tusinski@gmail.com>
2026-03-04 23:03:32 +0100

    Update to .NET 10, upgrade dependencies, adjust endpoints
    Upgraded all projects to .NET 10 and updated key NuGet packages, including Aspire.Hosting.AppHost, Yarp.ReverseProxy, and OpenTelemetry libraries. Commented out .WithHttpEndpoint calls in Program.cs to modify HTTP endpoint configuration. Added ASPIRE_ALLOW_UNSECURED_TRANSPORT to launchSettings.json. Minor formatting changes in BffGateway.csproj.

--- src/Aspire.AppHost/Program.cs
+++ src/Aspire.AppHost/Program.cs
@@ -1,13 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var catalogApi = builder.AddProject<Projects.CatalogApi>("catalogapi")
-    .WithHttpEndpoint(name: "http");
+var catalogApi = builder.AddProject<Projects.CatalogApi>("catalogapi");
+//.WithHttpEndpoint(name: "http");
 
-var ordersApi = builder.AddProject<Projects.OrdersApi>("ordersapi")
-    .WithHttpEndpoint(name: "http");
+var ordersApi = builder.AddProject<Projects.OrdersApi>("ordersapi");
+//.WithHttpEndpoint(name: "http");
 
 builder.AddProject<Projects.BffGateway>("bffgateway")
-    .WithHttpEndpoint(name: "http")
+    //.WithHttpEndpoint(name: "http")
     .WithEnvironment("Services__CatalogApi__BaseUrl", catalogApi.GetEndpoint("http"))
     .WithEnvironment("Services__OrdersApi__BaseUrl", ordersApi.GetEndpoint("http"))
     .WithReference(catalogApi)
